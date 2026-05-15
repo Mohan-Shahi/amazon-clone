ab37f66fb6a0e023b0df4c14d06515c8da5d1991
adidaplays <pmdmoonton470@gmail.com>
2026-05-15 22:14:35 +0545

    doing js part

--- files/cart.js
+++ files/cart.js
@@ -1,11 +1,10 @@
 
 let product_object= []
-
-
+let saved_value;
 
 let product_display = ()=>{
     product_object = product_list.map(item =>{
-
+        
         let product = products.find(product_item=>{
             return product_item.id == item.id 
         })
@@ -14,20 +13,23 @@ let product_display = ()=>{
             quantity:item.quantity
         }
     })
-
+    
     let item_number_display = document.getElementById('item_number')
     item_number_display.innerText =` ${item_count.length} items`
-    console.log(product_object)
-
-
-
+    // console.log(product_object)
+    
+    
+    
     let card_display = document.querySelector('.card-container')
     
     let newhtml = '';
     for(let i = 0;i<product_object.length;i++){
+        saved_value =  JSON.parse(localStorage.getItem(`delivery-${i}`))|| 'Tuesday, May 26';
         newhtml +=`
                 <div class="card">
-                    <div class="date">Delivery date: Tuesday, May 26</div>
+                    <div class="date">
+                    Delivery date: ${saved_value}
+                    </div>
                     <div class="item-detail">
                         <div class="image">
                             <img src="${product_object[i].image}" alt="">
@@ -44,26 +46,41 @@ let product_display = ()=>{
                         <div class="option">
                             <div class="head1">Choose a delivery option:</div>
                             <div class="option-1">
-                                <input type="radio" name="date" checked value="">
-                                <div>
+                                <input type="radio"
+                                    name="date-${i}"
+                                    value="Tuesday, May 26"
+                                    ${saved_value === 'Tuesday, May 26'
+                                        ? 'checked' : ''}>
+                                    <div>
                                     <p class="p1">Tuesday, May 26</p>
                                     <p class="p2">FREE Shipping</p>
-                                </div>
+                                    </div>
                             </div>
+                            
                             <div class="option-2">
-                                <input type="radio" name="date" value="">
-                                <div>
+                                <input type="radio"
+                                    name="date-${i}"
+                                    value="Wednesday, May 20"
+                                    ${saved_value === 'Wednesday, May 20'
+                                        ? 'checked' : ''}>
+                                    <div>
                                     <p class="p1">Wednesday, May 20</p>
                                     <p class="p2">FREE Shipping</p>
-                                </div>
+                                    </div>
                             </div>
+                           
                             <div class="option-3">
-                                <input type="radio" name="date" value="">
-                                <div>
+                                <input type="radio"
+                                    name="date-${i}"
+                                    value="Monday, May 18"
+                                    ${saved_value === 'Monday, May 18'
+                                        ? 'checked' : ''}>
+                                    <div>
                                     <p class="p1">Monday, May 18</p>
                                     <p class="p2">FREE Shipping</p>
-                                </div>
-                            </div>
+                                    </div>
+                             </div>
+                            
                             
                         </div>
                     </div>
@@ -72,10 +89,51 @@ let product_display = ()=>{
         `
     }
     card_display.innerHTML = newhtml;
+    
+}
+console.log(saved_value)
+
+
+function select_radio(){
+
+    const radios = document.querySelectorAll(
+        'input[type="radio"]'
+    );
+    
+    radios.forEach(radio => {
+        
+        
+        radio.addEventListener('change', (e) => {
+            
+            const card = e.target.closest('.card');
+            
+            const selected_radio = card.querySelector(
+                'input[type="radio"]:checked'
+            );
+            
+            
+            const date_display = card.querySelector('.date')
+            
+            const name = selected_radio.name;
+
+            date_display.innerText = `Delivery date: ${selected_radio.value}`
+            const index = name.split('-')[1];
+
+
+            saved_value = selected_radio.value
+            localStorage.setItem(
+                `delivery-${index}`,
+                JSON.stringify(saved_value));
+        });
+
+    });
+
 }
 
 
 function onload2(){
     product_display();
+    select_radio();
 }
+console.log(saved_value)
 onload2()
